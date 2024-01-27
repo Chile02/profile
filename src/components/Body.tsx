f84af44a58e8d85faf7c6a305f90edccad7402a3
Chile02 <blessingsmwandira4@gmail.com>
2024-01-27 19:11:17 +0200

    first commit

---
+++ src/components/Body.tsx
@@ -0,0 +1,10 @@
+function Body(){
+    return(
+        <div>
+            <p>
+                hello
+            </p>
+        </div>
+    )
+}
+export default Body
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header(){
+    return(
+        <header>
+         <nav>
+             <ul>
+                 <li><a href="#">Home</a></li>              
+             </ul>
+         </nav>
+     </header>
+    )
+}
+export default Header
